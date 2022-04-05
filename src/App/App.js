9f64193b085ba6e808fd4b67fff8e60c1f756733
Sunil Vishwakarma <sunilnet4@gmail.com>
2022-04-05 11:15:47 +0530

    feat: added login component

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import Login from "../Login/Login";
+import "./app.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="nav">
+
+      </div>
+
+      <Login />
+    </div>
+  );
+}
+
+export default App;
